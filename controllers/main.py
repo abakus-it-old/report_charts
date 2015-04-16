5439d7e1e95d618a2c25d4a3f677c1bc631461d9
Bernard Delhez <bernarddelhez@gmail.com>
2015-04-16 15:10:19 +0200

    Singleton figure fix

--- controllers/main.py
+++ controllers/main.py
@@ -1,4 +1,4 @@
-﻿from openerp.addons.web.http import Controller, route, request
+from openerp.addons.web.http import Controller, route, request
 
 class ReportChartController(Controller):
     @route(['/report/chart/pie'], type='http', auth="user")
@@ -20,19 +20,21 @@ class ReportChartController(Controller):
         if colors:
             colors = colors.split(",")
         else:
-            colors = ('b', 'g', 'r', 'c', 'm', 'y', 'k', 'w')
+            #colors = ('b', 'g', 'r', 'c', 'm', 'y', 'k', 'w')
+            #Orange colors
+            colors = ('#e55300','#ff6900','#ff5c00','#ff8106','#ff9f00','#ff2b00')
         if explode:
             explodeTmp = explode.split(",")
             explode = []
             for e in explodeTmp:
                 explode.append(float(e))
         
-        plt.figure(figsize=(8,4))
-        plt.pie(sizes, explode, labels, colors,autopct='%1.1f%%', shadow=False, startangle=90)
-        plt.axis('equal')
-        
-        fig = plt.gcf()
+        fig = plt.figure(figsize=(8,4))
         fig.patch.set_color('#FFFFFF')
+        ax = fig.add_subplot(111)
+        ax.pie(sizes, explode, labels, colors,autopct='%1.1f%%', shadow=False, startangle=90)
+        ax.axis('equal')
+        
         canvas = FigureCanvasAgg(fig)
         
         buf = cStringIO.StringIO()
@@ -41,9 +43,5 @@ class ReportChartController(Controller):
             data = buf.getvalue()
         finally:
             buf.close()
-        
-        plt.close('all')
-        plt.clf()
-        plt.cla()
         
         return request.make_response(data, headers=[('Content-Type', 'image/png')])
