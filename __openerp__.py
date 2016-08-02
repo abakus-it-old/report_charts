7f889b939114fdeadd61e1fe05b040d43d4b9d53
Valentin Thirion <valentinthirion@gmail.com>
2016-08-02 14:59:08 +0200

    Update __openerp__.py

--- __openerp__.py
+++ __openerp__.py
@@ -8,6 +8,11 @@
     'description': 
     """
     This modules adds charts for report. The Python library 'matplotlib' is required.
+
+    /!\ Need to install NUMPY on your server.
+    - apt-get install python-pip
+    - pip install numpy
+
     This module has been developed by Bernard Delhez, intern @ AbAKUS it-solutions, under the control of Valentin Thirion.
     """,
     'data': [],
