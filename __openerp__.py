473acdd32ab0def695c51aa45081cb2a1378eb3d
Bernard Delhez <bernarddelhez@gmail.com>
2016-08-03 08:43:51 +0200

    Changed version number

--- __openerp__.py
+++ __openerp__.py
@@ -1,14 +1,19 @@
 ﻿{
     'name': "Chart report",
-    'version': '1.0',
+    'version': '9.0.1.0',
     'depends': [],
     'author': "Bernard DELHEZ, AbAKUS it-solutions SARL",
     'website': "http://www.abakusitsolutions.eu",
     'category': 'Report',
     'description': 
     """
-    This modules adds charts for report. The Python library 'matplotlib' is required.
-    This module has been developed by Bernard Delhez, intern @ AbAKUS it-solutions, under the control of Valentin Thirion.
+This modules adds charts for report. The Python library 'matplotlib' is required.
+
+matplotlib installation:
+    - Debian / Ubuntu : sudo apt-get install python-matplotlib
+    - Fedora / Redhat : sudo yum install python-matplotlib
+
+This module has been developed by Bernard Delhez, intern @ AbAKUS it-solutions, under the control of Valentin Thirion.
     """,
     'data': [],
 }
